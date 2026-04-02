d92dd41e34e2778b278ca41add92145379026493
y.endo <y.endo@kinotrope.co.jp>
2026-04-02 22:38:24 +0900

    manifest.json の内容を TypeScript で定義できるように設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
 
-// https://vite.dev/config/
+// manifest.json の内容を TypeScript で定義（静的な json ファイルより管理が楽です）
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'VOICEVOX ポスト読み上げ',
+  version: '1.0.0',
+  permissions: ['storage'],
+  content_scripts: [
+    {
+      matches: ['https://gsm-app.com/*'],
+      js: ['src/content/index.tsx'], // エントリポイントを指定
+    },
+  ],
+  options_page: 'options.html',
+})
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    crx({ manifest }),
+  ],
 })
